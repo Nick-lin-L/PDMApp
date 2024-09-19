995d2046ca2fd6d69d161473e1dc825b64767540
nick.lin <nick.lin@pouchen.com>
2024-09-19 13:45:14 +0800

    函示化

--- PDMApp/Controllers/SPECHeadController.cs
+++ PDMApp/Controllers/SPECHeadController.cs
@@ -25,109 +25,39 @@ namespace PDMApp.Controllers
 
         // GET: api/<SPECHeadController>
         [HttpGet]
-        public IEnumerable<pdm_spec_headDto> Get([FromQuery]SpecSearchParameter value)
+        public IEnumerable<pdm_spec_headDto> Get([FromQuery] SpecSearchParameter value)
         {
-            //SPEC綜合查詢-查詢功能
-            var result = (from ph in _pcms_Pdm_TestContext.pdm_product_head
-                          join pi in _pcms_Pdm_TestContext.pdm_product_item on ph.product_m_id equals pi.product_m_id
-                          join sh in _pcms_Pdm_TestContext.pdm_spec_head on pi.product_d_id equals sh.product_d_id
-                          join si in _pcms_Pdm_TestContext.pdm_spec_item on sh.spec_m_id equals si.spec_m_id
-                          select new pdm_spec_headDto
-                          {
-                              Year = ph.year,
-                              Season = ph.season,
-                              Stage = sh.stage,
-                              out_mold_no = ph.out_mold_no,
-                              Mold_no = (ph.out_mold_no + "/" + ph.mid_mold_no + "/" + ph.etc_mold_no).Trim('/'),
-                              Shfactory = sh.factory,
-                              Factory = (ph.factory1 + "," + ph.factory2 + "," + ph.factory3).Replace(",,", ","),
-                              Item_name_eng = ph.item_name_eng,
-                              Item_name_jpn = ph.item_name_jpn,
-                              Item_no = ph.item_no,
-                              Dev_no = ph.dev_no,
-                              Dev_color_disp_name = pi.dev_color_disp_name,
-                              Color_no = pi.color_no,
-                              //以下是不需要的
-                              Spec_m_id = sh.spec_m_id,
-                              Cbdlockmk = sh.cbdlockmk,
-                              Product_m_id = ph.product_m_id,
-                              Product_d_id = pi.product_d_id,
-                          }).Distinct();
+            var result = QuerySpecHead().Distinct();
 
             if (!string.IsNullOrWhiteSpace(value.Spec_m_id))
-            {
                 result = result.Where(ph => ph.Spec_m_id.Contains(value.Spec_m_id));
-            }
             if (!string.IsNullOrWhiteSpace(value.Factory))
-            {
                 result = result.Where(ph => ph.Factory == value.Factory);
-            }
             if (!string.IsNullOrWhiteSpace(value.EntryMode))
-            {
                 result = result.Where(ph => ph.Entrymode == value.EntryMode);
-            }
             if (!string.IsNullOrWhiteSpace(value.Season))
-            {
                 result = result.Where(ph => ph.Season == value.Season);
-            }
             if (!string.IsNullOrWhiteSpace(value.Year))
-            {
                 result = result.Where(ph => ph.Year == value.Year);
-            }
             if (!string.IsNullOrWhiteSpace(value.Item_No))
-            {
                 result = result.Where(ph => ph.Item_no == value.Item_No);
-            }
             if (!string.IsNullOrWhiteSpace(value.Color_No))
-            {
                 result = result.Where(ph => ph.Color_no == value.Color_No);
-            }
             if (!string.IsNullOrWhiteSpace(value.Dev_No))
-            {
                 result = result.Where(ph => ph.Dev_no == value.Dev_No);
-            }
             if (!string.IsNullOrWhiteSpace(value.Devcolorno))
-            {
                 result = result.Where(ph => ph.Dev_color_disp_name.Contains(value.Devcolorno));
-            }
             if (!string.IsNullOrWhiteSpace(value.Stage))
-            {
                 result = result.Where(ph => ph.Stage.Contains(value.Stage));
-            }
+
             return result.ToList();
         }
 
-            // GET api/<SPECHeadController>/5
-            [HttpGet("{id}")]
+        // GET api/<SPECHeadController>/5
+        [HttpGet("{id}")]
         public pdm_spec_headDto Get(string id)
         {
-            var result = (from ph in _pcms_Pdm_TestContext.pdm_product_head
-                          join pi in _pcms_Pdm_TestContext.pdm_product_item on ph.product_m_id equals pi.product_m_id
-                          join sh in _pcms_Pdm_TestContext.pdm_spec_head on pi.product_d_id equals sh.product_d_id
-                          join si in _pcms_Pdm_TestContext.pdm_spec_item on sh.spec_m_id equals si.spec_m_id
-                          where sh.spec_m_id == id
-                          select new pdm_spec_headDto
-                          {
-                              Year = ph.year,
-                              Season = ph.season,
-                              Stage = sh.stage,
-                              out_mold_no = ph.out_mold_no,
-                              Mold_no = (ph.out_mold_no + "/" + ph.mid_mold_no + "/" + ph.etc_mold_no).Trim('/'),
-                              Shfactory = sh.factory,
-                              Factory = (ph.factory1 + "," + ph.factory2 + "," + ph.factory3).Replace(",,", ","),
-                              Item_name_eng = ph.item_name_eng,
-                              Item_name_jpn = ph.item_name_jpn,
-                              Item_no = ph.item_no,
-                              Dev_no = ph.dev_no,
-                              Dev_color_disp_name = pi.dev_color_disp_name,
-                              Color_no = pi.color_no,
-                              //以下是不需要的
-                              Spec_m_id = sh.spec_m_id, 
-                              Cbdlockmk = sh.cbdlockmk,
-                              Product_m_id = ph.product_m_id,
-                              Product_d_id = pi.product_d_id,
-                          }).FirstOrDefault();
-            return result;
+            return QuerySpecHead().FirstOrDefault(sh => sh.Spec_m_id == id);
         }
 
         // POST api/<SPECHeadController>
@@ -149,6 +79,33 @@ namespace PDMApp.Controllers
         }
 
 
-        
+        private IQueryable<pdm_spec_headDto> QuerySpecHead()
+        {
+            return (from ph in _pcms_Pdm_TestContext.pdm_product_head
+                    join pi in _pcms_Pdm_TestContext.pdm_product_item on ph.product_m_id equals pi.product_m_id
+                    join sh in _pcms_Pdm_TestContext.pdm_spec_head on pi.product_d_id equals sh.product_d_id
+                    join si in _pcms_Pdm_TestContext.pdm_spec_item on sh.spec_m_id equals si.spec_m_id
+                    select new pdm_spec_headDto
+                    {
+                        Year = ph.year,
+                        Season = ph.season,
+                        Stage = sh.stage,
+                        out_mold_no = ph.out_mold_no,
+                        Mold_no = (ph.out_mold_no + "/" + ph.mid_mold_no + "/" + ph.etc_mold_no).Trim('/'),
+                        Shfactory = sh.factory,
+                        Factory = (ph.factory1 + "," + ph.factory2 + "," + ph.factory3).Replace(",,", ","),
+                        Item_name_eng = ph.item_name_eng,
+                        Item_name_jpn = ph.item_name_jpn,
+                        Item_no = ph.item_no,
+                        Dev_no = ph.dev_no,
+                        Dev_color_disp_name = pi.dev_color_disp_name,
+                        Color_no = pi.color_no,
+                        Spec_m_id = sh.spec_m_id,
+                        Cbdlockmk = sh.cbdlockmk,
+                        Product_m_id = ph.product_m_id,
+                        Product_d_id = pi.product_d_id
+                    });
+        }
+
     }
 }
