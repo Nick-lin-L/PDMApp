17e00280db5eb7dd4f68ca8c0cb0ada12b530d79
nick.lin <nick.lin@pouchen.com>
2025-02-07 10:08:49 +0800

    修改參數名稱

--- PDMApp/Parameters/Basic/PermissionsParameter.cs
+++ PDMApp/Parameters/Basic/PermissionsParameter.cs
@@ -14,7 +14,7 @@ namespace PDMApp.Parameters.Basic
         public string? PermissionName { get; set; }
         public string? Description { get; set; }
 
-        public string? IncludeFactory { get; set; }
+        public string? DevFactoryNo { get; set; }
         //        public int PageNumber { get; set; } = 1; // 預設為第1頁
         //        public int PageSize { get; set; } = 10; // 預設每頁10筆
         public PaginationParameter Pagination { get; set; } = new PaginationParameter();
