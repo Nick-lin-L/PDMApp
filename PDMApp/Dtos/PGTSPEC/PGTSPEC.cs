e431981f8f4de1016925dff54c0f2eb944bff582
Allen Cheng 鄭名硯 <allen1.cheng@pouchen.com>
2025-02-27 08:15:44 +0800

    feat:SPEC匯出

--- PDMApp/Dtos/PGTSPEC/PGTSPEC.cs
+++ PDMApp/Dtos/PGTSPEC/PGTSPEC.cs
@@ -167,6 +167,46 @@ namespace Dtos.PGTSPEC
         public IEnumerable<SpecStandardDTO>? StandardData { get; set; }
     }
 
+    public class ItemSheetDTO
+    {
+        public string? SpecMId { get; set; }    //ID
+        public decimal? Seqno { get; set; }
+        public string? PartClass { get; set; }
+        public string? MailTo { get; set; }
+        public string? MailCC { get; set; }
+        public string? Stage { get; set; }
+        public string? CreateTime { get; set; }
+        public string? DevNo { get; set; }
+        public string? RefDevNo { get; set; }
+        public string? ItemNameEng { get; set; }
+        public string? ItemNo { get; set; }
+        public string? ColorNo { get; set; }
+        public string? DevelopmentColorNo { get; set; }
+        public string? SampleSize { get; set; }
+        public string? HeelHeight { get; set; }
+        public string? ColorNameChn { get; set; }
+        public string? ColorEng { get; set; }
+        public string? FactoryMoldNo1 { get; set; }
+        public string? FactoryMoldNo2 { get; set; }
+        public string? FactoryMoldNo3 { get; set; }
+        public string? LastNo1 { get; set; }
+        public string? LastNo2 { get; set; }
+        public string? LastNo3 { get; set; }
+        public string? CreateUser { get; set; }
+        public string? Type { get; set; }      //Newmaterial
+        public string? Parts { get; set; }
+        public string? No { get; set; }
+        public string? Material { get; set; }    // 材料
+        public string? SubMaterial { get; set; }    // 材料
+        public string? Colors { get; set; }
+        public string? Standard { get; set; }
+        public string? Hcha { get; set; }
+        public string? Sec { get; set; }
+        public string? Supplier { get; set; }    // 供應商
+        public object ActNo { get; internal set; }
+        public string RemarksProhibit { get; internal set; }
+    }
+
 
 }
 
