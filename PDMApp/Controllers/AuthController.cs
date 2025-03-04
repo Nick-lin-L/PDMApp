c767062eb3b7b89e140f793adf29c8149a1ec45f
nick.lin <nick.lin@pouchen.com>
2025-03-04 09:50:30 +0800

    login取得token。預計做後續接資料等動作

--- PDMApp/Controllers/AuthController.cs
+++ PDMApp/Controllers/AuthController.cs
@@ -8,7 +8,9 @@ using PDMApp.Configurations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Bartata.NET.WebForm;
 
 namespace PDMApp.Controllers
 {
@@ -31,60 +33,75 @@ namespace PDMApp.Controllers
         {
             var authProperties = new AuthenticationProperties
             {
-                RedirectUri = _config.RedirectUri
+                //RedirectUri = _config.RedirectUri
+                RedirectUri = Url.Action("Callback", "Auth", null, Request.Scheme) // 動態設定 Callback
             };
 
             // 傳回登入 URL，供前端重定向
             return Challenge(authProperties, OpenIdConnectDefaults.AuthenticationScheme);
         }
 
+
         /// <summary>
-        /// 取得目前登入的用戶資訊 (含Token) 
+        /// 登入資料回拋，交換Token、並返回用戶資料 OIDC
         /// </summary>
-        [HttpGet("me")]
-        public async Task<IActionResult> GetUserInfo()
+        /*
+        [HttpGet("signin-oidc")]
+        public IActionResult SigninOidc()
         {
-            var accessToken = await HttpContext.GetTokenAsync("access_token");
-            var idToken = await HttpContext.GetTokenAsync("id_token");
-
-            if (string.IsNullOrEmpty(accessToken))
+            var authProperties = new AuthenticationProperties
             {
-                return Unauthorized(new { error = "No valid token found" });
-            }
+                RedirectUri = _config.RedirectUri
+            };
 
-            return Ok(new
-            {
-                User = HttpContext.User.Identity.Name,
-                AccessToken = accessToken,
-                IdToken = idToken
-            });
-        }
+            // 傳回登入 URL，供前端重定向
+            return Challenge(authProperties, OpenIdConnectDefaults.AuthenticationScheme);
+        }*/
+
 
         /// <summary>
         /// 登入資料回拋，交換Token、並返回用戶資料
         /// </summary>
         [HttpGet("callback")]
-        public async Task<IActionResult> Callback()
+        //public async Task<IActionResult> Callback()
+        public async Task<IActionResult> Callback([FromQuery] string state)
         {
-            // 驗證結果
-            var authResult = await HttpContext.AuthenticateAsync(OpenIdConnectDefaults.AuthenticationScheme);
+            var uuid = Request.Query["uid"];    
+            var code = Request.Query["code"];
+            var State = Request.Query["state"];
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var idToken = await HttpContext.GetTokenAsync("id_token");
 
-            if (!authResult.Succeeded)
+            if (string.IsNullOrEmpty(accessToken))
             {
-                return Unauthorized(new { error = "Authentication failed" });
+                return BadRequest(new { error = "Authorization accessToken is missing" });
             }
 
-            // 取得 Token
-            var accessToken = authResult.Properties.GetTokenValue("access_token");
-            var idToken = authResult.Properties.GetTokenValue("id_token");
-            Console.WriteLine($"AccessToken: {accessToken}");
-            Console.WriteLine($"IdToken: {idToken}");
-            // 回傳 Token 給前端
-            return Ok(new
+            var tokenEndpoint = $"{_config.Authority}/protocol/openid-connect/token";
+            //https://iamlab.pouchen.com/auth/realms/pcg/protocol/openid-connect/userinfo 使用者帳戶資訊API
+            var clientId = _config.ClientId;
+            var clientSecret = _config.ClientSecret;
+            var redirectUri = _config.RedirectUri;
+
+            using var httpClient = new HttpClient();
+            var requestBody = new FormUrlEncodedContent(new Dictionary<string, string>
+                            {
+                                { "grant_type", "authorization_code" },
+                                { "client_id", clientId },
+                                { "client_secret", clientSecret },
+                                { "code", code },
+                                { "redirect_uri", redirectUri }
+                            });
+
+            var response = await httpClient.PostAsync(tokenEndpoint, requestBody);
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
             {
-                AccessToken = accessToken,
-                IdToken = idToken
-            });
+                return BadRequest(new { error = "Failed to exchange code for access token", details = responseContent });
+            }
+
+            return Ok(new { message = "Login successful", tokens = responseContent });
         }
 
         /// <summary>
