9e1ac12a1b6d3309d2fd410d533679f96629967b
nick.lin <nick.lin@pouchen.com>
2025-04-09 14:46:03 +0800

    修正Detail查詢條件

--- PDMApp/Parameters/ALink/SpecMIdParameter.cs
+++ PDMApp/Parameters/ALink/SpecMIdParameter.cs
@@ -11,6 +11,13 @@ namespace PDMApp.Parameters.ALink
 #nullable enable
         [Required]
         public string? SpecMId { get; set; }
+        public string? PartNo { get; set; }
+        public string? PartName { get; set; }
+        public string? MatColor { get; set; }
+        public string? Material { get; set; }
+        public string? SubMaterial { get; set; }
+        public string? Supplier { get; set; }
+        public string? Width { get; set; }
 
         //        public int PageNumber { get; set; } = 1; // 預設為第1頁
         //        public int PageSize { get; set; } = 10; // 預設每頁10筆
